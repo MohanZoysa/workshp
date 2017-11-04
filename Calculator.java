4e50f7a220ee7fb13bbf8aa98a23363cabf9e595
Mohan De Zoysa <mohan@crowderia.se>
2017-11-04 09:14:13 +0530

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+
+nckage org.com;
+
+import java.lang.*; 
+import java.util.*; 
+
+public class Calculator 
+{ 
+   
+    private static int x; 
+    private static int y; 
+    
+
+  
+
+    public int addition(int x, int y) 
+    { 
+       return x+y; 
+    } 
+    public int subtraction(int x, int y) 
+    { 
+       return 0; 
+    } 
+    public int multiplication(int x, int y) 
+    {     
+       return 0; 
+    } 
+    public int division(int x, int y) 
+    { 
+       return  0; 
+       
+    } 
+
+    
+    
+} bproject/private/
